0b97c3df8d9cc347b611166578cd92da21d1298e
Milk-Cool <milan-pl@mail.ru>
2024-07-30 16:30:09 +0300

    v2

--- gen/main.py
+++ gen/main.py
@@ -1,10 +1,14 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained('tinkoff-ai/ruDialoGPT-small')
-model = AutoModelForCausalLM.from_pretrained('tinkoff-ai/ruDialoGPT-small')
+modn = "ai-forever/rugpt3small_based_on_gpt2"
+tokenizer = AutoTokenizer.from_pretrained(modn)
+print("py: create tokenizer")
+model = AutoModelForCausalLM.from_pretrained(modn)
+print("py: create model")
 def gen(ctx):
     inputs = tokenizer(ctx, return_tensors='pt')
+    print("py: tokenizer done")
     generated_token_ids = model.generate(
         **inputs,
         top_k=10,
@@ -17,7 +21,9 @@ def gen(ctx):
         repetition_penalty=1.2,
         length_penalty=1.0,
         eos_token_id=50257,
-        max_new_tokens=800
+        max_new_tokens=100
     )
+    print("py: generated token ids")
     context_with_response = [tokenizer.decode(sample_token_ids) for sample_token_ids in generated_token_ids]
-    return context_with_response[0]
+    print("py: generated response")
+    return context_with_response[0][len(ctx):]
--- index.js
+++ index.js
@@ -2,29 +2,38 @@ import result from "./export/result.json" with { "type": "json" };
 import nodecallspython from "node-calls-python";
 import TelegramBot from "node-telegram-bot-api";
 import SegfaultHandler from "segfault-handler";
+console.log("node: import modules");
 
 SegfaultHandler.registerHandler("crash.log");
 
 const { TOKEN, CHANNEL } = process.env;
 const bot = new TelegramBot(TOKEN, { "polling": false });
+console.log("node: create bot");
 
 const py = nodecallspython;
 
-let { messages } = result;
-messages = messages.map(x => typeof x.text === "string" ? x.text : x.text.reduce((a, b) => a + (typeof b === "string" ? b : b.text), ""));
-messages = messages.slice(messages.length - 200);
-messages = messages.map(x => `@@POST@@${(() => {
-    x = x.slice(0, 200);
-    x = x.slice(0, x.lastIndexOf(/ |\./g));
-    return x;
-})()}`).join("");
-messages = messages.slice(0, 2000) + "@@POST@@";
-
 const pymodule = py.importSync("gen/main.py");
+console.log("node: import python");
 const gensend = async () => {
-    let generated = (await py.call(pymodule, "gen", messages));
-    generated = generated.split("@@").sort((a, b) => b.length - a.length)[0];
+    let { messages } = result;
+    messages = messages.map(x => typeof x.text === "string" ? x.text : x.text.reduce((a, b) => a + (typeof b === "string" ? b : b.text), ""));
+    const start = Math.floor(Math.random() * (messages.length - 200));
+    messages = messages.slice(start, start + 200);
+    messages = messages.map(x => `[${(() => {
+        x = x.slice(0, 200);
+        x = x.slice(0, x.lastIndexOf(/ |\./g));
+        return x;
+    })()}]`);
+    const msgInit = messages.map(x => x.trim());
+    messages = messages.join("");
+    messages = messages.slice(0, 2000) + "][";
+    console.log(messages)
+    console.log("node: load data");
+
+    let generated = (await py.call(pymodule, "gen", messages)).replace("]", "").replace(/[^^\.]+\. /, "");
+
     bot.sendMessage(parseInt(CHANNEL), generated);
+    console.log("node: parsed & sent response");
 };
 gensend();
 setInterval(gensend, 2 * 60 * 60 * 1000);
